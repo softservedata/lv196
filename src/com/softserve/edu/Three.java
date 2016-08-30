3dbd03ca5077e6d4796d063b76ee6d3f07c91898
Kurdiukov Taras <kurdyukov.taras@mail.ru>
2016-08-30 18:49:40 +0300

    1 patch

--- src/com/softserve/edu/Three.java
+++ src/com/softserve/edu/Three.java
@@ -2,7 +2,12 @@ package MainDirectory;
 
 // 562 Натуральное число из n цифр является числом Армстронга,если сумма его цифр, возведенных в n-ю степень, равна самому числу(как, например, 153 = 1^3+ 5^3+ 3^3). Получить все числа Армстронга,состоящие из двух, трех и четырех цифр.
 public class Three {
+
     public static void main(String[] args) {
+        Three three=new Three;
+        three.method();
+    }
+    public void method() {
         for(int i=10;i<9999;i++){
             char[] arr=String.valueOf(i).toCharArray();
 
@@ -15,10 +20,10 @@ public class Three {
                 for (char g:arr) {
                     System.out.print(" " + g);
                 }
-             System.out.println();
+                System.out.println();
             }
 
-    }
+        }
     }
 
 }
