ab6ef0b49253d7a1ef5cb06bf3234105d7e0397e
Kurdiukov Taras <kurdyukov.taras@mail.ru>
2016-08-30 18:54:23 +0300

    2 patch

--- src/com/softserve/edu/MainClass.java
+++ src/com/softserve/edu/MainClass.java
@@ -6,11 +6,11 @@ public class MainClass {
 
 
         public int sum(){
-            String s=n.toString();
-            char[] arr= s.toCharArray();
-            int summ=0;
+            String s=n.toString();    //З числа в строку
+            char[] arr= s.toCharArray();  //З строки в массив символів
+            int summ=0;                   // Ініціалізуемо змінну і прирівнюемо до 0
 
-            for (int i=arr.length-m; i<arr.length; i++){
+            for (int i=arr.length-m; i<arr.length; i++){   //беремо з массиву потрібні нам символи
             summ+=Integer.parseInt(((Character)arr[i]).toString());
             }
             return summ;
