b274060c9f32e78385c36a48ca922a1783dc9be6
lurqui21 <drugsandrock7@gmail.com>
2016-09-09 23:33:25 +0300

    add user entity

--- src/main/java/com/softserve/edu/delivery/domain/User.java
+++ src/main/java/com/softserve/edu/delivery/domain/User.java
@@ -1,4 +1,161 @@
 package com.softserve.edu.delivery.domain;
 
+import javax.persistence.*;
+import java.util.Objects;
+
+@Entity
+@Table(name = "USERS")
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "ID")
+    private Long id;
+
+    @Column(name = "EMAIL")
+    private String email;
+
+    @Column(name = "PASSWORD")
+    private String password;
+
+    @Column(name = "FIRST_NAME")
+    private String firstName;
+
+    @Column(name = "LAST_NAME")
+    private String lastName;
+
+    @Column(name = "PASSPORT")
+    private String passport;
+
+    @Column(name = "PHONE")
+    private String phone;
+
+    @Column(name = "RATE")
+    private Integer rate;
+
+    @Column(name = "RATE")
+    private String photoUrl;
+
+    @Enumerated
+    @Column(name = "USER_ROLE")
+    private Role userRole;
+
+    public enum Role {
+        ADMIN, MODERATOR, DRIVER, CUSTOMER
+    }
+
+    public User() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public User setId(Long id) {
+        this.id = id;
+        return this;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public User setEmail(String email) {
+        this.email = email;
+        return this;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public User setPassword(String password) {
+        this.password = password;
+        return this;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public User setFirstName(String firstName) {
+        this.firstName = firstName;
+        return this;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public User setLastName(String lastName) {
+        this.lastName = lastName;
+        return this;
+    }
+
+    public String getPassport() {
+        return passport;
+    }
+
+    public User setPassport(String passport) {
+        this.passport = passport;
+        return this;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public User setPhone(String phone) {
+        this.phone = phone;
+        return this;
+    }
+
+    public Integer getRate() {
+        return rate;
+    }
+
+    public User setRate(Integer rate) {
+        this.rate = rate;
+        return this;
+    }
+
+    public String getPhotoUrl() {
+        return photoUrl;
+    }
+
+    public User setPhotoUrl(String photoUrl) {
+        this.photoUrl = photoUrl;
+        return this;
+    }
+
+    public Role getUserRole() {
+        return userRole;
+    }
+
+    public User setUserRole(Role userRole) {
+        this.userRole = userRole;
+        return this;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return Objects.equals(id, user.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", email='" + email + '\'' +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                '}';
+    }
 }
