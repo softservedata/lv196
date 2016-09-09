3da9f7458b0c573556c77f55b614f9b9f3040987
yaroslav <garasym@gmail.com>
2016-09-09 20:24:12 +0300

    Template for maven project

---
+++ src/main/java/com/softserve/edu/delivery/App.java
@@ -0,0 +1,14 @@
+package com.softserve.edu.delivery;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+    
+}
