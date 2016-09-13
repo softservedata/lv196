a0e370ea7427d75b4831fb29dd2093b30479fc7a
Natalia <natala468@gmail.com>
2016-09-13 10:36:07 +0300

    change documents and delete approve

--- src/main/java/com/softserve/edu/delivery/domain/Car.java
+++ src/main/java/com/softserve/edu/delivery/domain/Car.java
@@ -15,6 +15,7 @@ import javax.persistence.Table;
 @Table(name = "CARS")
 public class Car {
 
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	@Column(name = "car_id")
@@ -27,14 +28,8 @@ public class Car {
 	private Double length;
 	@Column(name = "weight")
 	private Double weight;
-
-	@OneToOne
-	@JoinColumn(name = "document_id")
-	private Document document;
-
-	@Column(name = "isApproved")
-	private Boolean isApproved;
-
+	@Column(name = "document")
+	private String document;
 	@ManyToOne
 	@JoinColumn(name = "user_id")
 	private User user;
@@ -46,64 +41,63 @@ public class Car {
 		return user;
 	}
 
-	public void setUser(User user) {
+	public Car setUser(User user) {
 		this.user = user;
+		return this;
 	}
 
 	public Long getCarId() {
 		return carId;
 	}
 
-	public void setCarId(Long carId) {
+	public Car setCarId(Long carId) {
 		this.carId = carId;
+		return this;
 	}
 
 	public Double getWidth() {
 		return width;
 	}
 
-	public void setWidth(Double width) {
+	public Car setWidth(Double width) {
 		this.width = width;
+		return this;
 	}
 
 	public Double getHeight() {
 		return height;
 	}
 
-	public void setHeight(Double height) {
+	public Car setHeight(Double height) {
 		this.height = height;
+		return this;
 	}
 
 	public Double getLength() {
 		return length;
 	}
 
-	public void setLength(Double length) {
+	public Car setLength(Double length) {
 		this.length = length;
+		return this;
 	}
 
 	public Double getWeight() {
 		return weight;
 	}
 
-	public void setWeight(Double weight) {
+	public Car setWeight(Double weight) {
 		this.weight = weight;
+		return this;
 	}
 
-	public Document getDocument() {
+	public String getDocument() {
 		return document;
 	}
 
-	public void setDocumentId(Document document) {
+	public Car setDocument(String document) {
 		this.document = document;
-	}
-
-	public Boolean getIsApproved() {
-		return isApproved;
-	}
-
-	public void setIsApproved(Boolean isApproved) {
-		this.isApproved = isApproved;
+		return this;
 	}
 
 	@Override
@@ -124,7 +118,7 @@ public class Car {
 
 	@Override
 	public String toString() {
-		return "Car id: " + carId + "\nWidth: " + width + ", height: " + height + ", length: " + length + "\nWeight: " + weight + 
-			"\nDocument: " + document.toString() + "\nUser: " + user.toString();
+		return "Car id: " + carId + "\nWidth: " + width + ", height: " + height + ", length: " + length + 
+		"\nWeight: " + weight + "\nDocument: " + document + "\nUser: " + user.toString();
 	}
 }
