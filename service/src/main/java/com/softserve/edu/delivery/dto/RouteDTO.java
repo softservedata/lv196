b56d6391556679bcc11a09730735f61f568cd6c3
Petro Shtenovych <shtenyovych@live.com>
2016-09-15 13:16:33 +0300

    Update and realize RouteDTO class.

--- service/src/main/java/com/softserve/edu/delivery/dto/RouteDTO.java
+++ service/src/main/java/com/softserve/edu/delivery/dto/RouteDTO.java
@@ -2,6 +2,7 @@ package com.softserve.edu.delivery.dto;
 
 import com.softserve.edu.delivery.domain.Baggage;
 import com.softserve.edu.delivery.domain.City;
+import com.softserve.edu.delivery.domain.OrderStatus;
 import com.softserve.edu.delivery.domain.User;
 
 import java.sql.Timestamp;
@@ -23,7 +24,92 @@ public class RouteDTO {
 
     private User owner;
 
-    private User transporter;
+    private User transporter; //todo: in Order class(domain) add User as transporter
 
+    private OrderStatus orderStatus;
 
+    public RouteDTO(City lastLocation,
+                    Timestamp lastTimeVisited,
+                    List<City> visitedLocations,
+                    Baggage baggage,
+                    User owner,
+                    User transporter,
+                    OrderStatus orderStatus) {
+        this.lastLocation = lastLocation;
+        this.lastTimeVisited = lastTimeVisited;
+        this.visitedLocations = visitedLocations;
+        this.baggage = baggage;
+        this.owner = owner;
+        this.transporter = transporter;
+        this.orderStatus = orderStatus;
+    }
+
+    public RouteDTO() {}
+
+    /*<-------------Setters------------------>*/
+
+    public RouteDTO setLastLocation(City lastLocation) {
+        this.lastLocation = lastLocation;
+        return this;
+    }
+
+    public RouteDTO setLastTimeVisited(Timestamp lastTimeVisited) {
+        this.lastTimeVisited = lastTimeVisited;
+        return this;
+    }
+
+    public RouteDTO setVisitedLocations(List<City> visitedLocations) {
+        this.visitedLocations = visitedLocations;
+        return this;
+    }
+
+    public RouteDTO setBaggage(Baggage baggage) {
+        this.baggage = baggage;
+        return this;
+    }
+
+    public RouteDTO setOwner(User owner) {
+        this.owner = owner;
+        return this;
+    }
+
+    public RouteDTO setTransporter(User transporter) {
+        this.transporter = transporter;
+        return this;
+    }
+
+    public RouteDTO setOrderStatus(OrderStatus orderStatus) {
+        this.orderStatus = orderStatus;
+        return this;
+    }
+
+     /*<-------------Getters------------------>*/
+
+    public City getLastLocation() {
+        return lastLocation;
+    }
+
+    public Timestamp getLastTimeVisited() {
+        return lastTimeVisited;
+    }
+
+    public List<City> getVisitedLocations() {
+        return visitedLocations;
+    }
+
+    public Baggage getBaggage() {
+        return baggage;
+    }
+
+    public User getOwner() {
+        return owner;
+    }
+
+    public User getTransporter() {
+        return transporter;
+    }
+
+    public OrderStatus getOrderStatus() {
+        return orderStatus;
+    }
 }
