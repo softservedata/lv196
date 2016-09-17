a3a27b6a52ba6ead5e0b8ce6967f346a7f289927
Petro Shtenovych <shtenyovych@live.com>
2016-09-17 11:43:36 +0300

    Declare method in OfferDao interface which returns all offers by given order. Realize its implementation in OfferDaoImpl

--- persistence/src/main/java/com/softserve/edu/delivery/dao/OfferDao.java
+++ persistence/src/main/java/com/softserve/edu/delivery/dao/OfferDao.java
@@ -3,7 +3,12 @@ package com.softserve.edu.delivery.dao;
  * Author - Ivan Synyshyn
  */
 import com.softserve.edu.delivery.domain.Offer;
+import com.softserve.edu.delivery.domain.Order;
+
+import java.util.List;
 
 public interface OfferDao extends BaseDao<Offer, Long> {
 
+
+    List getAllOffersByOrder(Order order);
 }
--- persistence/src/main/java/com/softserve/edu/delivery/dao/impl/OfferDaoImpl.java
+++ persistence/src/main/java/com/softserve/edu/delivery/dao/impl/OfferDaoImpl.java
@@ -2,6 +2,11 @@ package com.softserve.edu.delivery.dao.impl;
 
 import com.softserve.edu.delivery.dao.OfferDao;
 import com.softserve.edu.delivery.domain.Offer;
+import com.softserve.edu.delivery.domain.Order;
+
+import javax.persistence.EntityManager;
+import javax.persistence.Query;
+import java.util.List;
 
 /**
  * Created by Ivan Rudnytskyi on 12.09.2016.
@@ -9,5 +14,18 @@ import com.softserve.edu.delivery.domain.Offer;
 public class OfferDaoImpl extends BaseDaoImpl<Offer, Long> implements OfferDao {
     public OfferDaoImpl() {
         super(Offer.class);
+    }
+
+
+    /**Method returns all offers by given order
+     * Author - Petro Shtenovych
+     * */
+    @Override
+    @SuppressWarnings("unchecked")
+    public List<Offer> getAllOffersByOrder(Order order) {
+        EntityManager em = super.getEntityManager();
+        Query query = em.createQuery("select of, o from Order of join of.offers o where o.id = :id ");
+        query.setParameter("id", order.getId());
+        return (List<Offer>) query.getResultList();
     }
 }
