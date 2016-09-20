681163364cc099e2c4ab5f79de43e0b577ef2218
Petro Shtenovych <shtenyovych@live.com>
2016-09-20 14:54:51 +0300

    Test

--- service/src/test/java/com/softserve/edu/delivery/service/tracking/RouteByIdTest.java
+++ service/src/test/java/com/softserve/edu/delivery/service/tracking/RouteByIdTest.java
@@ -7,15 +7,21 @@ import com.softserve.edu.delivery.dao.RouteCityDao;
 import com.softserve.edu.delivery.dao.impl.OfferDaoImpl;
 import com.softserve.edu.delivery.dao.impl.OrderDaoImpl;
 import com.softserve.edu.delivery.dao.impl.RouteCityDaoImpl;
+import com.softserve.edu.delivery.dao.impl.UserDaoImpl;
 import com.softserve.edu.delivery.domain.*;
+import com.softserve.edu.delivery.dto.RouteDTO;
 import com.softserve.edu.delivery.service.RouteService;
 import com.softserve.edu.delivery.service.impl.RouteServiceImpl;
 import com.softserve.edu.delivery.utils.Jpa;
 import org.mockito.Mockito;
+import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
 
 import javax.persistence.EntityManager;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Query;
 import java.math.BigDecimal;
 import java.sql.Timestamp;
 import java.util.ArrayList;
@@ -23,7 +29,7 @@ import java.util.List;
 
 /**
  * @author Petro Shtenovych
- * Testing method getRouteById(Long id)
+ * Testing method getRouteById(Long id) from RouteServieImpl class
  * Method should return RouteDTO class instance which represent all
  * necessary tracking information about order
  * */
@@ -39,10 +45,17 @@ public class RouteByIdTest {
             new RouteServiceImpl(orderDao, routeCityDao, offerDao);
 
 
+    //test entities
     private Order testOrder = new Order();
+    private City from = new City();
+    private City to = new City();
+    private User costumer = new User().setEmail("TestCostumer@domain.com").setPassword("Password123#");
+    private User transporter = new User().setEmail("Transporter@domain.com").setPassword("Password123#");
+    private Offer offer = new Offer();
+    private Car car = new Car();
 
     @BeforeClass
-    public void saveTestOrderToDatabase() {
+    public void saveTestEntitiesToDatabase() {
         BigDecimal baggageHeight = new BigDecimal(1.11);
         BigDecimal baggageWidth = new BigDecimal(2.22);
         BigDecimal baggageLength = new BigDecimal(3.33);
@@ -50,53 +63,68 @@ public class RouteByIdTest {
         BigDecimal baggagePrice = new BigDecimal(5.55);
         Timestamp registrationDate = new Timestamp(10_000_000L);
         Timestamp arrivalDate = new Timestamp(20_000_000L);
-        String description = "It's testing order";
-        Order order = new Order();
-        order.setHeight(baggageHeight);
-        order.setWidth(baggageWidth);
-        order.setLength(baggageLength);
-        order.setWeight(baggageWeight);
-        order.setPrice(baggagePrice);
-        order.setRegistrationDate(registrationDate);
-        order.setArrivalDate(arrivalDate);
-        order.setDescription(description);
-        City from = new City(); from.setCityName("Lviv");
-        City to = new City(); to.setCityName("Kyiv");
-        order.setCityFrom(from);
-        order.setCityTo(to);
+        String description = "TEST";
+        testOrder.setHeight(baggageHeight);
+        testOrder.setWidth(baggageWidth);
+        testOrder.setLength(baggageLength);
+        testOrder.setWeight(baggageWeight);
+        testOrder.setPrice(baggagePrice);
+        testOrder.setRegistrationDate(registrationDate);
+        testOrder.setArrivalDate(arrivalDate);
+        testOrder.setDescription(description);
+        from.setCityName("Lviv");
+        to.setCityName("Kyiv");
+        testOrder.setCityFrom(from);
+        testOrder.setCityTo(to);
         List<Order> orders = new ArrayList<>();
-        orders.add(order);
+        orders.add(testOrder);
 
-        User costumer = new User().setEmail("TestCostumer@domain.com").setPassword("Password123#");
         costumer.setOrders(orders);
-
         List<Offer> offers = new ArrayList<>();
-        Offer offer = new Offer();
         offer.setOrder(this.testOrder);
-        Car car = new Car();
         offer.setCar(car);
         offer.setApproved(true);
         offers.add(offer);
-
-        order.setOffers(offers);
-
-        User transporter = new User().setEmail("Transporter@domain.com").setPassword("Password123#");
+        testOrder.setOffers(offers);
         List<Car> cars = new ArrayList<>();
         cars.add(car);
         transporter.setCars(cars);
         car.setDriver(transporter);
 
-
+        EntityTransaction tx = null;
+        try {
+            tx = em.getTransaction();
+            tx.begin();
+            new UserDaoImpl().save(this.costumer);
+            new UserDaoImpl().save(this.transporter);
+            new OrderDaoImpl().save(this.testOrder);
+            new OfferDaoImpl().save(this.offer);
+            tx.commit();
+        } catch (RuntimeException ex) {
+            if (tx != null) tx.rollback();
+            Jpa.close();
+            throw ex;
+        }
     }
 
-    @BeforeClass
-    public void createTestOrders() {
-
-    }
-
-    @AfterClass
-    public void removeTestOrders() {
-
+    @Test(enabled = false)
+    public void getRouteByIdTest() {
+        EntityTransaction tx = null;
+        try {
+            tx = em.getTransaction();
+            tx.begin();
+            Query query = em.createQuery("select o from Order o where o.description = :description", Order.class);
+            query.setParameter("description", "TEST");
+            Order order = (Order) query.getSingleResult();
+            Long orderId = order.getId();
+            //test method
+            RouteDTO dto = routeService.getRouteById(orderId);
+            System.out.println(dto);
+            tx.commit();
+        }catch (Exception e) {
+            if (tx != null) tx.rollback();
+            throw e;
+        }
     }
 
 
