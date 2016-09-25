923c980abc53a36ef6cabfda0043f45a0f934343
KurdiulovTaras <Vikavika1>
2016-09-25 17:15:19 +0300

    add test file.

--- service/src/test/java/com/softserve/edu/delivery/service/order/OrderServiceImplMockTest.java
+++ service/src/test/java/com/softserve/edu/delivery/service/order/OrderServiceImplMockTest.java
@@ -1,12 +1,12 @@
 package com.softserve.edu.delivery.service.order;
 
-import com.softserve.edu.delivery.dao.CityDao;
-import com.softserve.edu.delivery.dao.FeedbackDao;
-import com.softserve.edu.delivery.dao.OrderDao;
-import com.softserve.edu.delivery.dao.UserDao;
+import com.softserve.edu.delivery.dao.*;
 import com.softserve.edu.delivery.service.FeedbackService;
 import com.softserve.edu.delivery.service.OrderService;
+import com.softserve.edu.delivery.service.impl.FeedbackServiceImpl;
+import com.softserve.edu.delivery.service.impl.OrderServiceImpl;
 import org.mockito.Mockito;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 /**
@@ -17,16 +17,23 @@ public class OrderServiceImplMockTest {
     private OrderDao mockODao = Mockito.mock(com.softserve.edu.delivery.dao.OrderDao.class);
     private UserDao mockUDao = Mockito.mock(com.softserve.edu.delivery.dao.UserDao.class);
     private CityDao mockCDao = Mockito.mock(com.softserve.edu.delivery.dao.CityDao.class);
+    private OfferDao mockOfDao = Mockito.mock(com.softserve.edu.delivery.dao.OfferDao.class);
 
     private FeedbackService fsi;
     private OrderService os;
 
-    public OrderServiceImplMockTest(){
-
+    @BeforeClass
+    public void injectMockDAO() {
+        fsi = new FeedbackServiceImpl(mockFDao);
+        os = new OrderServiceImpl(mockODao,mockUDao,mockCDao,mockFDao,mockOfDao);
     }
 
     @Test
-    public void testName() throws Exception {
+    public void testChangeStatus() throws Exception {
+
+    }
+    @Test
+    public void testAddFeedback() throws Exception {
 
     }
 
