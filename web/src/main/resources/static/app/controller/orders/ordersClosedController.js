5b272696a36fcb1c813aa620870a16b57f2987ce
KurdiulovTaras <Vikavika1>
2016-10-05 19:19:36 +0300

    add orderService

--- web/src/main/resources/static/app/controller/orders/ordersClosedController.js
+++ web/src/main/resources/static/app/controller/orders/ordersClosedController.js
@@ -1,19 +1,20 @@
-angular
+    angular
     .module('delivery')
-    .controller('ordersClosedController', ['$scope', '$http', '$uibModal', 
+    .controller('ordersClosedController',
+        ['$scope', '$http', '$uibModal',
         function ($scope, $http, $uibModal) {
         $scope.orders = {
             closed: []
         };
-            
+
         $scope.retrieveClosedOrders = () => {
             $http.get('/order/closed').then(response => {
                 $scope.orders.closed = response.data;
             })
         };
         $scope.retrieveClosedOrders();
-            
-        $scope.addFeedback = () => {
+
+        $scope.addFeedback = function () {
             const modalInstance = $uibModal.open({
                 animation: true,
                 templateUrl: '/app/views/add.feedback.html',
@@ -21,14 +22,18 @@ angular
             });
 
         }
-    }])
-    .controller('addFeedbackController', ['$scope', '$http', '$uibModalInstance',
-        function ($scope, $http, $uibModalInstance) {
+        }]
+    )
+    .controller('addFeedbackController' ['$scope', '$http', '$uibModalInstance',
+        function ($scope, $http, $uibModalInstance, orderService) {   //визиваю мій Сервіс
+            $scope.idForFeedback = orderService.getId();              //витягую з Сервісу та призначаю Id змінній
+
             $scope.form = {
                 submit: () => {
                     const data = {
                         rate: $scope.form.rate,
-                        text: $scope.form.text
+                        text: $scope.form.text,
+                        orderId: $scope.idForFeedback      //призначаю orderId відгуку
                     };
                     $http.post('/order/addfeedback', data).then(response => {
                         $uibModalInstance.close(true)
@@ -41,4 +46,17 @@ angular
             $scope.cancel = function () {
                 $uibModalInstance.dismiss('cancel');
             };
-        }]);
+        }]
+    )
+    .service('orderService', function () {
+        var _id = 3;                         //Захардкодив orderId для перевірки
+        return {
+            setId: function (id) {
+                _id = id;
+            },
+            getId: function () {
+                return _id;
+            }
+        }
+    });
+
