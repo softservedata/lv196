b62587d6e0c4218facb1303ade8353a2e5187087
KurdiulovTaras <Vikavika1>
2016-10-27 22:15:20 +0300

    Change UI for closed orders. Change UI for notifications - add uib-accordion for different status. Add new words language files

--- web/src/main/resources/static/app/notification/controller/notificationController.js
+++ web/src/main/resources/static/app/notification/controller/notificationController.js
@@ -3,17 +3,35 @@ angular
     .controller('notificationController',['$scope', '$http', 'Notification','$notificationProperty', '$rootScope',
         function ($scope, $http, Notification, $notificationProperty, $rootScope) {
             $scope.notifications = {
-                notifications: []
+                notifications: [],
+                info: [],
+                warning: [],
+                success: []
             };
 
             $scope.retrieveNotifications = () => {
                 $http.get('/notification').then(response => {
-                    $scope.notifications = response.data;
+                    $scope.notifications.notifications = response.data;
+                    $scope.notifications.info.length = 0;
+                    $scope.notifications.success.length = 0;
+                    $scope.notifications.warning.length = 0;
+                    for (var i=0;i<$scope.notifications.notifications.length;i++){
+                        if ($scope.notifications.notifications[i].notificationStatus === 'Info'){
+                            $scope.notifications.info.push($scope.notifications.notifications[i]);
+                        }
+                        else if ($scope.notifications.notifications[i].notificationStatus === 'Success'){
+                            $scope.notifications.success.push($scope.notifications.notifications[i]);
+                        }
+                        else if ($scope.notifications.notifications[i].notificationStatus === 'Warning'){
+                            $scope.notifications.warning.push($scope.notifications.notifications[i]);
+                        }
+                    }
+                    console.log('$scope.notifications.info.length = ' + $scope.notifications.info.length);
                     if ($notificationProperty.getAmount() > 0) {
                         ($rootScope.lang === 'en') ? Notification('Now all your notifications are readed'):
                             Notification('Всі ваші сповіщення зараз прочитані');
                     }
-                })
+                });
             };
             $scope.retrieveNotifications();
 
