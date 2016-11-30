3910fae5a5dd73b4a54527e39b5e64393f10750b
ivanrudnytskyi <ivan.rudn@gmail.com>
2016-11-30 18:31:35 +0200

    Fixed import.sql (orders with feedback).

--- web/src/main/java/com/softserve/edu/delivery/controller/UserProfileController.java
+++ web/src/main/java/com/softserve/edu/delivery/controller/UserProfileController.java
@@ -11,7 +11,6 @@ import com.softserve.edu.delivery.service.UserService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
